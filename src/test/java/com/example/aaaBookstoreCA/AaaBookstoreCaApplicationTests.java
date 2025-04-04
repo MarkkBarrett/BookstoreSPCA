45931eb95df79b029fc94c3a3a48400c21b5c2ec
Mark Barrett <mbarrett@stmc.ie>
2025-04-04 15:40:47 +0100

    basic setup of Spring Framework and packages

---
+++ src/test/java/com/example/aaaBookstoreCA/AaaBookstoreCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aaaBookstoreCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaaBookstoreCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
